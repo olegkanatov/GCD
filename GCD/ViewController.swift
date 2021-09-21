972dac9c0721c606a1d12c20c340eff5ef3a686d
Oleg Kanatov <olegkanatov7@gmail.com>
2021-09-21 14:03:43 +0300

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Oleg Kanatov on 21.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
